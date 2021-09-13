5c7dbbd8b22694cda49df99fd339314d73a6ae3f
Yuji Yokoo <yujiyokoo@gmail.com>
2021-09-14 00:41:23 +0900

    Update game state from bool to a struct with a single bool

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,6 +11,10 @@ import FRP.Yampa as Yampa
 import SDL as SDL
 import SDL.Vect
 
+data GameState = GameState
+  { finished :: Bool
+  }
+
 runGame :: IO ()
 runGame = do
   renderer <- initScreen
@@ -18,32 +22,37 @@ runGame = do
   timeRef <- newIORef t
   reactimate initialise (input timeRef) (output renderer) isTimeUp
 
-initialise :: IO Bool
+initialise :: IO GameState
 initialise = do
   putStrLn "Starting..."
-  return False
+  return GameState { finished = False }
 
-input :: IORef UTCTime -> Bool -> IO (DTime, Maybe Bool)
+input :: IORef UTCTime -> Bool -> IO (DTime, Maybe GameState)
 input ref _ = do
   currTime <- getCurrentTime
   lastTime <- readIORef ref
   writeIORef ref currTime
   let dt = diffUTCTime currTime lastTime
-  return (realToFrac dt, Just True)
+  return (realToFrac dt, Nothing)
 
-output :: Renderer -> Bool -> Bool -> IO Bool
-output renderer _ x = do
+output :: Renderer -> Bool -> GameState -> IO Bool
+output renderer _ gs = do
   events <- pollEvents
   rendererDrawColor renderer $= V4 32 32 32 255
   clear renderer
+  -- draw a red square
   rendererDrawColor renderer $= V4 192 32 32 255
   fillRect renderer (Just (Rectangle (P (V2 100 100)) (V2 20 20)))
   present renderer
-  when x (putStrLn "Done")
-  return x
+  when (finished gs) (putStrLn "Done")
+  return (finished gs)
 
-isTimeUp :: SF Bool Bool
-isTimeUp = Yampa.time >>> arr (> 4)
+isTimeUp :: SF GameState GameState
+-- time :: SF a Time, arr 'lifts' a regular func to SF, >>> is SF bind
+isTimeUp =
+  Yampa.time >>> arr (\t ->
+                       let isTime = t > 4
+                       in GameState { finished = isTime })
 
 initScreen :: IO Renderer
 initScreen = do
