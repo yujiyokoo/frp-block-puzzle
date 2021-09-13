4bf792d0b5aa0cd39cd006b5201aa181efb00e06
Yuji Yokoo <yujiyokoo@gmail.com>
2021-09-13 17:20:06 +0900

    Add showing a blank grey window

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,52 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
-    ( someFunc
+    ( runGame
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Control.Monad
+import Data.IORef
+import Data.Time.Clock
+import FRP.Yampa as Yampa
+import SDL as SDL
+
+runGame :: IO ()
+runGame = do
+  renderer <- initScreen
+  t <- getCurrentTime
+  timeRef <- newIORef t
+  reactimate initialise (input timeRef) (output renderer) isTimeUp
+
+initialise :: IO Bool
+initialise = do
+  putStrLn "Starting..."
+  return False
+
+input :: IORef UTCTime -> Bool -> IO (DTime, Maybe Bool)
+input ref _ = do
+  currTime <- getCurrentTime
+  lastTime <- readIORef ref
+  writeIORef ref currTime
+  let dt = diffUTCTime currTime lastTime
+  return (realToFrac dt, Just True)
+
+output :: Renderer -> Bool -> Bool -> IO Bool
+output renderer _ x = do
+  events <- pollEvents
+  rendererDrawColor renderer $= V4 32 32 32 255
+  clear renderer
+  present renderer
+  when x (putStrLn "Done")
+  return x
+
+isTimeUp :: SF Bool Bool
+isTimeUp = Yampa.time >>> arr (> 4)
+
+initScreen :: IO Renderer
+initScreen = do
+  initializeAll
+  window <- createWindow "My SDL Application" defaultWindow
+  renderer <- createRenderer window (-1) defaultRenderer
+  return renderer
+
+
