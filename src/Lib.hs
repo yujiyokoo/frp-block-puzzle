48b98af64cc9ff25662077cc12dce541821007b5
Yuji Yokoo <yujiyokoo@gmail.com>
2021-09-18 14:18:28 +0900

    Implement simple keyboard read

--- src/Lib.hs
+++ src/Lib.hs
@@ -16,7 +16,22 @@ data GameState = GameState
   , startTime :: UTCTime
   , timePassed :: DTime
   , frameNum :: Int
+  , buttonPresses :: ButtonPresses
   }
+
+data ButtonPresses = ButtonPresses
+  { upArrow :: Bool
+  , downArrow :: Bool
+  , leftArrow :: Bool
+  , rightArrow :: Bool
+  , rotateL :: Bool
+  , rotateR :: Bool
+  , drop :: Bool
+  }
+  deriving (Show, Eq)
+
+noButtonPressed :: ButtonPresses
+noButtonPressed = ButtonPresses False False False False False False False
 
 runGame :: IO ()
 runGame = do
@@ -31,17 +46,48 @@ initialise = do
   currTime <- getCurrentTime
   -- let timePassed = diffUTCTime currTime (startTime gs)
   -- putStrLn ("frameChanged: " ++ (show (frameChanged gs)) ++ ", " ++ (show timePassed))
-  return GameState { finished = False, startTime = currTime, timePassed = 0, frameNum = 0 }
+  return GameState { finished = False, startTime = currTime, timePassed = 0, frameNum = 0, buttonPresses = noButtonPressed }
 
 input :: IORef (UTCTime, UTCTime) -> Bool -> IO (DTime, Maybe GameState)
 input ref _ = do
   currTime <- getCurrentTime
   (start, lastTime) <- readIORef ref
   writeIORef ref (start, currTime)
+  events <- pollEvents
   let dt = diffUTCTime currTime lastTime
       timePassed = realToFrac (diffUTCTime currTime start)
       frameNum = (round (timePassed * 60)) `mod` 60
-  return (realToFrac dt, Just (GameState {finished = False, startTime = start, timePassed = timePassed, frameNum = frameNum}))
+      buttonPresses = buttonPressesFrom events
+      gameState = GameState {finished = False, startTime = start, timePassed = timePassed, frameNum = frameNum, buttonPresses = buttonPresses}
+  return (realToFrac dt, Just gameState)
+
+buttonPressesFrom :: [SDL.Event] -> ButtonPresses
+buttonPressesFrom events =
+  let u = any (checkKeyPress [ScancodeUp]) events
+      d = any (checkKeyPress [ScancodeDown]) events
+      l = any (checkKeyPress [ScancodeLeft]) events
+      r = any (checkKeyPress [ScancodeRight]) events
+      shift = any (checkKeyPress [ScancodeLShift, ScancodeRShift]) events
+      ctrl = any (checkKeyPress [ScancodeLCtrl, ScancodeRCtrl]) events
+      sp = any (checkKeyPress [ScancodeSpace]) events
+  in
+  ButtonPresses u d l r shift ctrl sp
+
+checkKeyPress :: [Scancode] -> SDL.Event -> Bool
+checkKeyPress scanCodes event =
+  case eventPayload event of
+    KeyboardEvent keData ->
+      isPress keData && any (\code -> scancodeOf keData == code) scanCodes
+    _ ->
+      False
+
+isPress :: KeyboardEventData -> Bool
+isPress ked =
+  keyboardEventKeyMotion ked == Pressed
+
+scancodeOf :: KeyboardEventData -> Scancode
+scancodeOf ked =
+  keysymScancode $ keyboardEventKeysym ked
 
 output :: Renderer -> Bool -> GameState -> IO Bool
 output renderer _ gs = do
@@ -54,7 +100,9 @@ output renderer _ gs = do
   present renderer
   currTime <- getCurrentTime
   let tp = timePassed gs
+      bps = buttonPresses gs
   putStrLn ("timePassed: " ++ (show tp) ++ ", frameNum: " ++ (show $ frameNum gs))
+  when (bps /= noButtonPressed) (putStrLn ("Buttons: " ++ (show $ bps)))
   when (finished gs) (putStrLn "Done")
   return (finished gs)
 
